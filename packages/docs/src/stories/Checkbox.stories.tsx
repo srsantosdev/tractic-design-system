6a8cb0acdffc3003ed08d7f26c658e9fefa52961
Samuel Ramos <srsantosdev@gmail.com>
2023-06-25 18:44:35 -0300

    feat(react): add checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,21 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@tractic/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+      >
+        <Story />
+        <Text size="sm">Accept terms of use</Text>
+      </Box>
+    ),
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
